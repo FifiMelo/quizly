e5a82444a1d4668ec5170c98b49822156c4f5dd2
FifiMelo <felipe336336@gmail.com>
2024-01-20 23:04:47 +0100

    fake answers

---
+++ fake_answers_generator.py
@@ -0,0 +1,52 @@
+import openai
+import json
+
+client = openai.OpenAI(
+    api_key="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"
+)
+
+conversation = [
+    {
+        "role": "system",
+        "content": """Your task is to create answers to a python script puzzle,
+        one, that is a true answers, and three, that are wrong, but looks like correct.
+        You will be given python script.
+        Please give the answer in format:
+        correct_answer: *what python script returns*
+        fake_answer1: *first fake answer*
+        fake_answer2: *second fake answer*
+        fake_answer3: *third fake answer*
+        """
+    },
+    {
+        "role": "user",
+        "content": "Please, tell me what should return the following scripts, as well as the fake answers"
+    }
+]
+
+while True:
+    text = input()
+    if text in ["quit", "exit"]:
+        print("Do you want to save conversation to the file?")
+        if input() in ["yes", "Y", "y"]:
+            print("Provide name file: (without extension)")
+            name_file = input()
+            with open(f'{name_file}.json', 'w') as fp:
+                json.dump(conversation, fp)
+        break
+    conversation.append({
+        "role": "user",
+        "content": text
+    })
+
+    stream = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        #model="gpt-4",
+        messages=conversation,
+    )
+    answer = stream.choices[0].message.content
+    print(answer)
+    conversation.append({
+        "role": "assistant",
+        "content": answer
+    })
