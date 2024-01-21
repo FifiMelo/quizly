8a987394fbd4344434a9851b99c91d173774db4e
FifiMelo <felipe336336@gmail.com>
2024-01-21 22:47:45 +0100

    removed main from bot.py file

--- bot.py
+++ bot.py
@@ -51,19 +51,3 @@ class Bot(openai.OpenAI):
         return answer
 
 
-if __name__ == '__main__':
-    Question_generator = Bot(
-        system_info=""""Your task is to create python puzzle chunk. 
-        The chunk should end in statement result = ..., variable that will store the result of the code.
-        Please, don't provide anything else than the code, no questions, no answers, no hints, nothing.
-        """,
-        continuous=True
-    )
-    print(Question_generator.get(
-        """
-        Give me one python puzzle involving python. It should have at most 5 lines of code.
-        Difficulty should be around 25, on a sclty should be around 25, on a scale 1-100.
-        Store the value to be estlty should be around 25, on a scale 1-100.
-        Store the value to be estimated in variable "result"."""
-        ))
-    print(Question_generator.get("A bit harder one, please"))
