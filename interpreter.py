a027f01cf74fe6896011fe07b0f8522972fe095e
FifiMelo <felipe336336@gmail.com>
2024-01-25 23:10:52 +0100

    moved answers_correct function

--- interpreter.py
+++ interpreter.py
@@ -15,4 +15,18 @@ def get_real_answer(code: str):
     
     return  result_dict["result"], True
     
-#TODO write the function that interprets bots answers for different types
+def answers_correct(real_answer, answers):
+    """
+    Answers from chatgpt will always come as strings, but they can represent all possible
+    types, numbers, arrays, dictionaries ect.
+    Purpose of this function is to compare these answers, 
+    and return True only if all of them are correct.
+    """
+    answer_dict = dict()
+    for answer in answers:
+
+        # strings need to be treated separately (purpose of try catching)
+        exec(f"try:\n\tvar = {answer}\nexcept ValueError:\n\tvar = '{answer}'", answer_dict)
+        if not answer_dict["var"] == real_answer:
+            return False
+    return True
