3d53c6179643f62ef77144de039d6e17487b3b90
FifiMelo <felipe336336@gmail.com>
2024-01-25 23:31:01 +0100

    handling answer in bad form

--- interpreter.py
+++ interpreter.py
@@ -1,4 +1,4 @@
-
+from icecream import ic
 
 def get_real_answer(code: str):
 
@@ -23,10 +23,15 @@ def answers_correct(real_answer, answers):
     and return True only if all of them are correct.
     """
     answer_dict = dict()
-    for answer in answers:
+    for index in range(len(answers)):
 
+        # chatgpt often answers this way
+        if not answers[index].find('The value of the "result" variable at the end is') == -1:
+            answers[index] = answers[index][len('The value of the "result" variable at the end is '):-1]
+            ic(answers[index])
+            pass
         # strings need to be treated separately (purpose of try catching)
-        exec(f"try:\n\tvar = {answer}\nexcept ValueError:\n\tvar = '{answer}'", answer_dict)
+        exec(f"try:\n\tvar = {answers[index]}\nexcept ValueError:\n\tvar = '{answers[index]}'", answer_dict)
         if not answer_dict["var"] == real_answer:
             return False
     return True
