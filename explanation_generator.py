cf729e5dee83d32e601372d58a325eb09597c280
FifiMelo <felipe336336@gmail.com>
2024-01-26 23:26:09 +0100

    removed tabs from multiline strings

--- explanation_generator.py
+++ explanation_generator.py
@@ -5,18 +5,18 @@ class ExplanationGenerator(Bot):
     def __init__(self):
         super(ExplanationGenerator, self).__init__(
             system_info = """
-            Your task is to explain what python script does,
-            and then predict what is the value of variable "result".
-            Answer should be only value of the variable "result", do not provide anything else.
-            Please give answer in format:
-            explanation: *explanation*
-            answer: *what is the value of varaible "result" at the end*
-            do not provide anything more
-            """,
+Your task is to explain what python script does,
+and then predict what is the value of variable "result".
+Answer should be only value of the variable "result", do not provide anything else.
+Please give answer in format:
+explanation: *explanation*
+answer: *what is the value of varaible "result" at the end*
+do not provide anything more
+""",
             additional_info = """
-            Please, explain me what the script does, 
-            and tell what is the value of varaible "result" at the end.
-            """,
+Please, explain me what the script does, 
+and tell what is the value of varaible "result" at the end.
+""",
             model = "gpt-3.5-turbo",
             continuous = False
         )
--- question_generator.py
+++ question_generator.py
@@ -5,11 +5,11 @@ class QuestionGenerator(Bot):
     def __init__(self, tag: str):
         super(QuestionGenerator, self).__init__(
             system_info = """
-            Your task is to create python puzzle chunk. 
-            The chunk should end in statement result = ..., variable that will store the result of the code.
-            Please, don't provide anything else than the code, no questions, no answers, no hints, nothing.
-            Please, don't use random numbers, the puzzle must be deterministically answerable.
-            """,
+Your task is to create python puzzle chunk. 
+The chunk should end in statement result = ..., variable that will store the result of the code.
+Please, don't provide anything else than the code, no questions, no answers, no hints, nothing.
+Please, don't use random numbers, the puzzle must be deterministically answerable.
+""",
             additional_info = None,
             model = "gpt-3.5-turbo",
             continuous = True,
@@ -25,22 +25,22 @@ class QuestionGenerator(Bot):
         ):
         if self.number_of_generated_questions == 0:
             request = f"""
-                Give me one python puzzle involving {self.tag}. It should have at most 5 lines of code.
-                Store the value to be estimated in variable "result".
-                """
+Give me one python puzzle involving {self.tag}. It should have at most 5 lines of code.
+Store the value to be estimated in variable "result".
+"""
         else:
             if difficulty_change > 0:
                 request = """
-                    Very nice, but this time please give me something slightly more challenging.
-                    """
+Very nice, but this time please give me something slightly more challenging.
+"""
             if difficulty_change < 0:
                 request = """
-                    Very nice, but this one was too hard. Please give me something a little bit easier.
-                    """
+Very nice, but this one was too hard. Please give me something a little bit easier.
+"""
             if difficulty_change == 0:
                 request = """
-                    Very nice, please give me next one, on same difficulty level.
-                    """
+Very nice, please give me next one, on same difficulty level.
+"""
         self.number_of_generated_questions += 1
         return self.get(
             request = request
