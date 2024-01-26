7c38f9af55fb1706ef52de1696798c7c558ec354
FifiMelo <felipe336336@gmail.com>
2024-01-26 23:34:38 +0100

    created generate_fake_answers method

--- fake_answers_generator.py
+++ fake_answers_generator.py
@@ -9,7 +9,7 @@ Your task is to create answers to a python script puzzle,
 one, that is a true answers, and three, that are wrong, but looks like correct.
 You will be given python script.
 Please give the answer in format:
-correct_answer: *what python script returns*
+correct_answer: *what python script really returns*
 fake_answer1: *first fake answer*
 fake_answer2: *second fake answer*
 fake_answer3: *third fake answer*     
@@ -20,4 +20,27 @@ Please, tell me what should return the following scripts, as well as the fake an
             model = "gpt-3.5-turbo",
             continuous = False
         )
+    
+    
+    def generate_fake_answers(
+            self,
+            question_code: str
+    ):
+        query = self.get(question_code)
+
+        real_answer_index = query.find("correct_answer: ")
+        fake_answer1_index = query.find("fake_answer1: ")
+        fake_answer2_index = query.find("fake_answer2: ")
+        fake_answer3_index = query.find("fake_answer3: ")
+
+        real_answer = query[real_answer_index:fake_answer1_index]
+        fake_answer1 = query[fake_answer1_index:fake_answer2_index]
+        fake_answer2 = query[fake_answer2_index:fake_answer3_index]
+        fake_answer3 = query[fake_answer3_index:]
+
+        return real_answer, [fake_answer1, fake_answer2, fake_answer3]
         
+
+
+
+
