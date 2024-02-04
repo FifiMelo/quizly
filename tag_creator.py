3c9c11798ced83550f023e219a1cbf42bb29f07c
FifiMelo <felipe336336@gmail.com>
2024-02-04 21:36:16 +0100

    created tags creator

---
+++ tag_creator.py
@@ -0,0 +1,23 @@
+from bot import Bot
+from icecream import ic
+
+class TagCreator(Bot):
+    def __init__(self):
+        super(TagCreator, self).__init__(
+            system_info = """
+Your task is to provide tags for a python script.
+You will be given python script.
+There should be 3 - 6 tags.
+Please give the answer in format:
+tags: *tags separated with comas*
+Do not provide anyting else than tags.
+""",
+            additional_info = "Please, give me the tags about this script"
+        )
+    
+    def create_tags(self, question):
+        query = self.get(
+            request = question
+        )
+        return query.split(',')
+    
