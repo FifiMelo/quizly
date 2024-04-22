b8385fd3ed81992bfcaa5031fd9558a3440208ab
FifiMelo <felipe336336@gmail.com>
2024-04-22 21:14:59 +0200

    structure change

--- archive/a.py
+++
@@ -1,9 +0,0 @@
-a = 17
-b = 5
-result = a % b
-result = (result + b) % b
-result = (result - b) % b
-result = (result * b) % b
-result = (result ** b) % b
-
-print(result)
--- archive/interpreter.py
+++
@@ -1,5 +0,0 @@
-
-
-result = 0
-exec('(17 ** 5) % 6')
-print(result)
